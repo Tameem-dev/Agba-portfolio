3027b55d15c049ff54894f054860aba37fdcd5e0
Temim Opadijo <tameemopadijo23@gmail.com>
2026-08-11 00:08:59 +0100

    Fix Chucks Kitchen image import

--- src/pages/CaseStudy/CaseStudy.jsx
+++ src/pages/CaseStudy/CaseStudy.jsx
@@ -14,7 +14,7 @@ import finflow from '../../assets/finflow.png';
 import nocturne from '../../assets/nocturne.png';
 import devbridge from '../../assets/devbridge.png';
 import foodbridge from '../../assets/foodbridge.png';
-import chucksKitchen from "../../assets/chucks-Kitchen.png";
+import chucksKitchen from "../../assets/chucks-kitchen.png";
 import estatein from '../../assets/estatien.png';
 
 const projectImages = {
